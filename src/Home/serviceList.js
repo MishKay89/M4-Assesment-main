f11b823749c63de8f0aed6f1d719d1cc32574a76
MishKay89 <104592311+MishKay89@users.noreply.github.com>
2022-09-10 23:13:27 +0200

    code added

--- src/Home/serviceList.js
+++ src/Home/serviceList.js
@@ -2,27 +2,27 @@ const ServiceList = [
     {
         name: "Bitcoin Talk",
         email: "crypto@gmail.com",
-        review: '37',
+        review: '22',
         coord:{latitude:-32.914058,longitude:17.990005}
     },
     {
         name: "NFT Chat",
         email: "nft@gmail.com",
-        review: '48',
+        review: '45',
         coord:{latitude:-32.903393,longitude:18.002716}
 
 
     }, {
         name: "Photography",
         email: "camera@gmail.com",
-        review: '36',
+        review: '43',
         coord:{latitude:-32.903393,longitude:17.994471}
 
 
     }, {
         name: "Cash Flow Optimization",
         email: "money@gmail.com",
-        review: '14',
+        review: '60',
         coord:{latitude:-32.916076,longitude:17.991379}
     },
 ]
