75885b5a19640fdd09ed337809c87462395a2620
MishKay89 <104592311+MishKay89@users.noreply.github.com>
2022-09-10 22:58:42 +0200

    Initial commit

---
+++ src/Home/serviceList.js
@@ -0,0 +1,30 @@
+const ServiceList = [
+    {
+        name: "Bitcoin Talk",
+        email: "crypto@gmail.com",
+        review: '37',
+        coord:{latitude:-32.914058,longitude:17.990005}
+    },
+    {
+        name: "NFT Chat",
+        email: "nft@gmail.com",
+        review: '48',
+        coord:{latitude:-32.903393,longitude:18.002716}
+
+
+    }, {
+        name: "Photography",
+        email: "camera@gmail.com",
+        review: '36',
+        coord:{latitude:-32.903393,longitude:17.994471}
+
+
+    }, {
+        name: "Cash Flow Optimization",
+        email: "money@gmail.com",
+        review: '14',
+        coord:{latitude:-32.916076,longitude:17.991379}
+    },
+]
+
+export default ServiceList;
